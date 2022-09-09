a7dddb457804765edec6f03d1df9f210d36c906a
Wanogho Confidence <wanoghoco@gmail.com>
2022-09-09 10:34:27 +0100

    fixing android bugs

--- android/src/main/java/com/selfieliveness/selfie_liveness/SelfieLivenessPlugin.java
+++ android/src/main/java/com/selfieliveness/selfie_liveness/SelfieLivenessPlugin.java
@@ -2,37 +2,65 @@ package com.selfieliveness.selfie_liveness;
 
 import androidx.annotation.NonNull;
 
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
 /** SelfieLivenessPlugin */
-public class SelfieLivenessPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+public class SelfieLivenessPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
+
+  ActivityPluginBinding binding;
+  private SelfieDelegate delegate;
   private MethodChannel channel;
+
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "selfie_liveness");
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "elatech_liveliness_plugin");
     channel.setMethodCallHandler(this);
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    if (call.method.equals("detectliveliness")) {
+      detectLiveness(call,result);
     }
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+
     channel.setMethodCallHandler(null);
+  }
+
+  private void detectLiveness(MethodCall call,Result result){
+    delegate.detectLivelinesss(call, result);
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+  this.binding=binding;
+  this.delegate= new SelfieDelegate(binding.getActivity());
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
   }
 }
